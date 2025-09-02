b9dbc891ea86ed9d20959450a6d15f30ef7217e0
carlita_v <varela_carlita@live.fr>
2025-09-02 12:49:25 +0200

    premier commit - initialisation du projet Ecoride

---
+++ Public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ECORIDE</title>
+    <link rel="stylesheet" href="./ASSET/CSS/styles.css">
+</head>
+<body>
+
+<!--------------START - HEADER------------->
+<header>
+    <div class="container">
+        <div class="titleAcceuil">
+            <h1 class="titre">En route pour un covoiturage écologique !</h1>
+            <p class="slogan">"Chaque place partagée, une planète soulagée" 
+                Moin cher. Moin seul. Moin de CO².
+            </p>
+        </div>
+        <div class="imgAcceuil">
+            <img src="./ASSET/IMAGES/moment_de_partage_4.png" alt="trajet en voiture">
+        </div>
+
+    </div>
+</header>
+    
+    
+</body>
+</html>
