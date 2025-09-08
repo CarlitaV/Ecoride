7b6b15dbc93104e5d0307e981c4c36e797724d2a
carlita_v <varela_carlita@live.fr>
2025-09-08 16:07:05 +0200

    Ajout de la page d'acceuil

---
+++ vue/accueil.php
@@ -0,0 +1,15 @@
+
+    <section class="containerAcceuil">
+
+        <div class="titleAcceuil">
+            <h1 class="titre">En route pour un covoiturage écologique !</h1>
+            <p class="slogan">"Chaque place partagée, une planète soulagée" 
+                Moin cher. Moin seul. Moin de CO².
+            </p>
+        </div>
+
+        <div class="imgAcceuil">
+            <img src="/Public/ASSET/IMAGES/moment_de_partage_4.png" alt="trajet en voiture">
+        </div>
+
+    </section>
