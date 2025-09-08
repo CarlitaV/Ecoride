01dffaf9a083dedbc408ec0a108122e9cf775851
carlita_v <varela_carlita@live.fr>
2025-09-08 16:04:39 +0200

    Ajout de la navbar

---
+++ vue/layout/navbar.php
@@ -0,0 +1,32 @@
+
+    <!--------------START - NAV BAR------------->
+    <nav class="menu">
+        <div class="logo">
+            <img src="./ASSET/IMAGES/Logo-ECORIDE.png" alt="Logo ECORIDE">
+        </div>
+        <div class="box">
+            <ul>
+                <li><a href="?page=accueil">Accueil</a></li>
+                <li><a href="?page=listecovoiturage">Liste des covoiturages</a></li>
+                <li><a href="#Apropos">À propos</a></li>
+                <li><a href="#Contact">Contact</a></li>
+            </ul>
+        </div>
+        <div class="recherche">
+            <input type="text" placeholder="Recherche...">
+        </div>
+        
+        <div class="auth-bouton">
+
+            <div class="boutonMenu">
+                <button class="connexion" type="button"><a href="?page=connexion">Connexion</a></button> <!--Verrifer le type de bouton -->
+                <button class="inscription" type="button"><a href="?page=inscription">Inscription</a></button>
+            </div>
+
+            <div class="boiteVerte">
+            </div>
+
+        </div>
+
+    </nav>
+<!--------------END - NAV BAR------------->
