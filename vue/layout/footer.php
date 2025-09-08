e2151acc363f187902ef27b75a00b44988b0034d
carlita_v <varela_carlita@live.fr>
2025-09-08 16:05:22 +0200

    Ajout du pied de page

---
+++ vue/layout/footer.php
@@ -0,0 +1,33 @@
+
+    <footer class="footer">
+        <div conteneur>
+            <div class="footer-gauche">
+                <h3>Ecoride</h3>
+                <p>Roulez éco-responable, partagez vos trajets !</p>
+            </div>
+            <div class="footer-centrale">
+                <h4>Liens rapides</h4>
+                <ul>
+                    <li><a href="index.php"> Accueil</a></li>
+                    <li><a href=""></a>Covoiturages</li>
+                    <li><a href=""></a>Contact</li>
+                    <li><a href=""></a>Connexion</li>
+                    <li><a href=""></a>Inscription</li>
+                </ul>
+            </div>
+            <div class="footer-droite">
+                <h3> Contact</h3>
+                <p>Email : contact@ecoride.com</p>
+                <p>Téléphone : +33 6 11 22 33 44 </p>
+                <div class="reseaux-sociaux">
+                    <a href="https://www.facebook.com" src="./ASSET/IMAGES/facebookicone.png" Facebook"></a>
+                    <a href="https://www.x.com" src="./ASSET/IMAGES/twittericone.png" alt="X"></a>
+                    <a href="https://www.instagram.com" src="./ASSET/IMAGES/instagramicone.png" alt="Instagram"></a>
+                </div>
+            </div>
+            <div class="footer-bas">
+                <p>copyrigt © 2025 Ecoride. Tous droits réservés.</p>
+            </div>
+        </div>
+        <small></small>
+    </footer>
