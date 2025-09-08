1a99dcc35a68dedb9bba6bf771a273d583e3d7da
carlita_v <varela_carlita@live.fr>
2025-09-08 16:08:11 +0200

    Ajout de la page d'inscription

---
+++ vue/inscription.php
@@ -0,0 +1,48 @@
+
+    <section class="form-conteneur">
+        <h2>Formulaire d'inscription</h2>
+        <form action="" method="POST">
+            <div>
+                <label for="nom">Votre nom</label>
+                <input type="text" name="nom" placeholder="Nom" required>
+            </div>
+
+            <div>
+                <label for="prenom">Prénom</label>
+                <input type="text" name="prenom" placeholder="Prénom" required>
+            </div>
+
+            <div>
+                <label for="adresse">Adresse</label>
+                <input type="text" name="adresse" placeholder="Adresse" required>
+            </div>
+
+            <div>
+                <label for="ville">Ville</label>
+                <input type="text" name="ville" placeholder="Ville" required>
+            </div>
+
+            <div>
+                <label for="code_postal">Code Postal</label>
+                <input type="text" name="code_postal" placeholder="Code Postal" required>
+            </div>
+
+            <div>
+                <label for="">Numero de téléphone</label>
+                <input type="text" name="telephone" placeholder="Nunéro de téléphone" required>
+            </div>
+
+            <div>
+                <label for="">Email</label>
+                <input type="text" name="email" placeholder="Email" required>
+            </div>
+
+            <div>
+                <label for="">Date de naissance</label>
+                <input type="text" name="date_naissance" placeholder="Date de naissance" required>
+            </div>
+
+            <button type="submit">Inscription</button>
+        </form>
+        <p>Vous avez déjà un compte ? <a href="connexion.php">Connectez-vous</a></p>
+    </section>
