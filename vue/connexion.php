560f648753e454de9a0b0ca3b79dbd9c80fbca4a
carlita_v <varela_carlita@live.fr>
2025-09-08 16:07:38 +0200

    Ajout de la page de connection

---
+++ vue/connexion.php
@@ -0,0 +1,26 @@
+
+    <div class="connexion-conteneur">
+
+        <div class="form-conteneur" id="connectionFrom">
+            <div class="form-tab">
+                <button id="connection"><a href="connexion.php">Connection</a></button>
+                <button id="inscritption"><a href="inscription.php">Inscription</a> </button>
+            </div>
+        </div>
+
+        <h2>Je me connecte :</h2>
+        
+        <form action="" method="post">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="mot_de_passe" placeholder="Mot de passe">
+            <div class="checkbox-conteneur">
+                <input type="checkbox" name="se_souvenir" id="checkbox">
+                <label for="checkbox">Se souvenir de moi</label>
+            </div>
+            <button type="submit" class="boutton-connexion"> Se connecter</button>
+        </form>
+
+        <p>Pas encore de compte ? <a href="inscription.php">S'inscrire</a></p>
+
+    </div>
+
